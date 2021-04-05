3bec33566304f8c02eb23f18204709f049be21e8
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2021-04-05 15:07:07 -0300

    Header

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,21 @@
+import React from 'react'
+import { Image } from 'react-native'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack';
 import Feed from './pages/Feed'
+import logo from './assets/instagram.png'
 
 const Routes = createAppContainer(
     createStackNavigator({
         Feed
+    }, {
+        defaultNavigationOptions: { // configuracoes pra todas as telas
+            headerLayoutPreset: 'center', // forçar a logo a ficar no centro
+            headerTitle: <Image source={logo} />, // titulo do header com o component nativo de imagem e seu source
+            headerStyle:{ //passando uma estilizacao pro header
+                backgroundColor: '#f5f5f5'
+            }
+        }
     })
 )
 
