f35fda73fafdc2ac2145f10b11c40c492cbb687f
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2021-04-05 14:41:26 -0300

    Estrutura e Nav

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack';
+import Feed from './pages/Feed'
+
+const Routes = createAppContainer(
+    createStackNavigator({
+        Feed
+    })
+)
+
+export default Routes;
