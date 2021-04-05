79b36b706b9619216f9966bcace55c9e837877c3
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2021-04-05 15:34:53 -0300

    Post e Styled

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import { View } from 'react-native'
+import { Post, PostHeader } from './styles'
 
 export default function Feed() {
     return (
-        <View />
+        <View>
+            <Post>
+                <PostHeader>
+                    <Avatar source="https://avatars.githubusercontent.com/u/77861206?v=4"/>
+                    <Name>noronhaax</Name>
+                </PostHeader>
+                <PostBody>
+                    <PostBodyImage source="https://user-images.githubusercontent.com/77861206/106316870-2e5fd100-624c-11eb-9647-05b58a922afe.png" />
+                </PostBody>
+                <PostFooter>
+                    <PostFooterDescription>
+                        <Name>noronhaax</Name>
+                        <Description>Ola, olha meu novo clone!</Description>
+                    </PostFooterDescription>
+                </PostFooter>
+            </Post>
+        </View>
     )
 }
---
+++ src/pages/Feed/styles.js
@@ -0,0 +1,38 @@
+import Styled from 'styled-components/native'
+
+export const Post = Styled.View`
+
+`;
+
+export const PostHeader = Styled.View`
+
+`;
+
+export const Avatar = Styled.Image`
+
+`;
+
+
+export const Name = Styled.Text`
+
+`;
+
+export const PostBody = Styled.View`
+
+`;
+
+export const PostBodyImage = Styled.Image`
+
+`;
+
+export const PostFooter = Styled.View`
+
+`;
+
+export const PostFooterDescription = Styled.View`
+
+`;
+
+export const Description = Styled.Text`
+
+`;
