5cb678ff8be1406337993462f527866681bf813c
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2021-04-06 00:27:09 -0300

    Estilizacao, Imagens e Scroll

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,25 +1,68 @@
 import React from 'react'
 import { View } from 'react-native'
-import { Post, PostHeader, Avatar, Name, PostBody, PostBodyImage, PostFooter, PostFooterDescription, Description } from './styles'
+import { PostList, Post, PostHeader, Avatar, Name, PostBody, PostBodyImage, PostFooter, PostFooterIcons, PostFooterDescription, Description } from './styles'
 
 export default function Feed() {
+    const imagemAvatar = "https://avatars.githubusercontent.com/u/77861206?v=4"
+    const imagemBody = "https://user-images.githubusercontent.com/77861206/108550523-70b98280-72cd-11eb-8cb1-62b0dee4c1c1.PNG"
     return (
         <View>
-            <Post>
-                <PostHeader>
-                    <Avatar source="https://avatars.githubusercontent.com/u/77861206?v=4"/>
-                    <Name>noronhaax</Name>
-                </PostHeader>
-                <PostBody>
-                    <PostBodyImage source="https://user-images.githubusercontent.com/77861206/106316870-2e5fd100-624c-11eb-9647-05b58a922afe.png" />
-                </PostBody>
-                <PostFooter>
-                    <PostFooterDescription>
+            <PostList>
+                <Post>
+                    <PostHeader>
+                        <Avatar source={{ uri: imagemAvatar }} />
                         <Name>noronhaax</Name>
-                        <Description>Ola, olha meu novo clone!</Description>
-                    </PostFooterDescription>
-                </PostFooter>
-            </Post>
+                    </PostHeader>
+                    <PostBody>
+                        <PostBodyImage source={{ uri: imagemBody }} />
+                    </PostBody>
+                    <PostFooter>
+                        <PostFooterIcons>
+
+                        </PostFooterIcons>
+                        <PostFooterDescription>
+                            <Name>noronhaax</Name>
+                            <Description>Ola, olha meu novo clone!</Description>
+                        </PostFooterDescription>
+                    </PostFooter>
+                </Post>
+                <Post>
+                    <PostHeader>
+                        <Avatar source={{ uri: imagemAvatar }} />
+                        <Name>noronhaax</Name>
+                    </PostHeader>
+                    <PostBody>
+                        <PostBodyImage source={{ uri: imagemBody }} />
+                    </PostBody>
+                    <PostFooter>
+                        <PostFooterIcons>
+
+                        </PostFooterIcons>
+                        <PostFooterDescription>
+                            <Name>noronhaax</Name>
+                            <Description>Ola, olha meu novo clone!</Description>
+                        </PostFooterDescription>
+                    </PostFooter>
+                </Post>
+                <Post>
+                    <PostHeader>
+                        <Avatar source={{ uri: imagemAvatar }} />
+                        <Name>noronhaax</Name>
+                    </PostHeader>
+                    <PostBody>
+                        <PostBodyImage source={{ uri: imagemBody }} />
+                    </PostBody>
+                    <PostFooter>
+                        <PostFooterIcons>
+
+                        </PostFooterIcons>
+                        <PostFooterDescription>
+                            <Name>noronhaax</Name>
+                            <Description>Ola, olha meu novo clone!</Description>
+                        </PostFooterDescription>
+                    </PostFooter>
+                </Post>
+            </PostList>
         </View>
     )
 }
--- src/pages/Feed/styles.js
+++ src/pages/Feed/styles.js
@@ -1,38 +1,62 @@
 import Styled from 'styled-components/native'
 
-export const Post = Styled.View`
 
+export const PostList = Styled.ScrollView.attrs({
+    showsVerticalScrollIndicator: false
+  })`
+    
+`;
+
+export const Post = Styled.View`
+    
 `;
 
 export const PostHeader = Styled.View`
-
+    margin-top: 20px;
+    display: flex;
+    flex-direction: row;
 `;
 
 export const Avatar = Styled.Image`
-    width: 100%;
+    height: 50px;
+    width: 50px;
+    border-radius: 50px;
 `;
 
 
 export const Name = Styled.Text`
-
+    margin-top: 10px;
+    margin-left: 5px;
+    font-weight: bold;
+    font-size: 17px;
 `;
 
 export const PostBody = Styled.View`
-
+    margin-top: 10px;
 `;
 
 export const PostBodyImage = Styled.Image`
-
+    height: 210px;
+    width: 400px;
 `;
 
 export const PostFooter = Styled.View`
-
+   
 `;
 
-export const PostFooterDescription = Styled.View`
+export const PostFooterIcons = Styled.View`
+    display:flex;
+    flex-direction: row;
+`;
 
+
+export const PostFooterDescription = Styled.View`
+    display:flex;
+    flex-direction: row;
 `;
 
 export const Description = Styled.Text`
-
+    margin-top: 13px;
+    margin-left: 10px;
+    font-size: 15px;
 `;
